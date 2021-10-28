999891727c16a1ad983292af3ef8596c25283122
Diana Beatriz Tinoco <dianabeatriztinoco@gmail.com>
2021-10-28 18:54:22 -0500

    halfway

--- src/scripts/api-util.js
+++ src/scripts/api-util.js
@@ -1,14 +1,19 @@
 //you comment back in one of the imports below, if you prefer axios or jquery for AJAX requests
 
 // import axios from 'axios';
-// import $ from 'jquery';
+//import $ from 'jquery';
+
+const fetch = require('node-fetch')
 
 // `getAllUsers` should
 // (1) fetch all users from https://jsonplaceholder.typicode.com/users and
 // (2) return the response as an array of user objects
 // (hint: the api returns users as a stringified JSON array)
 export const getAllUsers = () => {
-  // Your code here
+  const response = fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => data)
+  return response
 };
 
 // `getUserPosts` should
@@ -17,7 +22,10 @@ export const getUserPosts = (userId) => {
   // (2) fetch all of that user's posts from `https://jsonplaceholder.typicode.com/posts?userId=${userId}` and
   // (3) return the response as an array of post objects
   // (hint: the api returns posts as a stringified JSON array)
-  // Your code here
+  const response = fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+  .then(res => res.json())
+  .then(data => data)
+return response
 };
 
 // `getPostComments` should
@@ -26,7 +34,8 @@ export const getPostComments = (postId) => {
   // (2) fetch all of that post's posts from `https://jsonplaceholder.typicode.com/posts/${postId}/comments` and
   // (3) return the response as an array of comment objects
   // (hint: the api returns comments as a stringified JSON array)
-  // Your code here
+  const response = fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+  .then(res => res.json())
+  .then(data => data)
+return response
 };
-
-// Your code here
--- src/scripts/render-users.js
+++ src/scripts/render-users.js
@@ -4,12 +4,35 @@ import renderPosts from "./render-posts";
 export async function renderUsers() {
   // it should:
   // (1) invoke getAllUsers to fetch all users from the API
+  const users = await getAllUsers()
   // Your code here
   // (2) alphabetize the users
+  
+  const alphabeticalUsers = users.sort(function(a,b){
+    return a.username.localeCompare(b.username)
+  })
+  const userList = document.getElementsByClassName('users-list')
+  const userUsername = document.createElement('p')
+  const userCity = document.createElement('p')
+  alphabeticalUsers.map((userObj)=>{
+    userUsername.innerHTML = userObj.username
+    userCity.innerHTML = userObj.address.city
+    userList.appendChild(userUsername)
+  })
+
+
   // (3) generate a collection of HTML elements (one for every user)
+ 
+
+  // userUsername.innerHTML = alphabeticalUsers.username
+  // userCity.innerHTML = alphabeticalUsers.city 
+
   // (3a) each element should contain a paragraph with the user's username and a paragraph with the user's city
   // (3b) each element should have a click event listener attached that
   //    calls renderPosts with an argument of user.id
   //(4) append the collection of user elements to the `article.users-list`
 }
+
+window.onload = renderUsers
+
 
